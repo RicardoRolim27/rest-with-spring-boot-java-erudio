c121e6853e38a971a20032a488027eb782016057
ricardo.oliveira <ricardo.oliveira@aliare.co>
2025-11-27 13:28:41 -0300

    Add JPA and PostgreSQL integration for Person entity
    Introduced Spring Data JPA and PostgreSQL dependencies, configured database connection in application.yml, annotated Person model as a JPA entity, and created PersonRepository for data access. These changes enable persistence and retrieval of Person entities using PostgreSQL.

--- src/main/java/br/com/ricardorolim/model/Person.java
+++ src/main/java/br/com/ricardorolim/model/Person.java
@@ -1,21 +1,31 @@
 package br.com.ricardorolim.model;
 
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
 private static final long serialVersionUID = 1l;
 
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 
+@Column(name = "first_name", nullable = false, length = 80)
 private String name;
 
+@Column(name = "last_name", nullable = false, length = 80)
 private String lastName;
 
+@Column(name = "gender", nullable = false, length = 6)
 private String gender;
 
+@Column(name = "adress", nullable = false, length = 100)
 private String address;
 
 public Person() {};
